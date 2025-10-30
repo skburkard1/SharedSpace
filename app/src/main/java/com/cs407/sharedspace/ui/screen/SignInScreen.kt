e2bbb1e984cfdb5c354fe17db5c3b43743f7e9f8
itssmea <quicksilvershedinja@gmail.com>
2025-10-30 15:47:37 -0500

    Personal branch for Samantha

--- app/src/main/java/com/cs407/sharedspace/ui/screen/SignInScreen.kt
+++ app/src/main/java/com/cs407/sharedspace/ui/screen/SignInScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+//import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
